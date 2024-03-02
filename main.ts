c56d813451fb08df676167fb4439a68679a16883
Urooj Muhammad Khalid <161545173+Urooj-Khalid123@users.noreply.github.com>
2024-03-02 21:41:02 +0500

    first commit

---
+++ main.ts
@@ -0,0 +1,8 @@
+//Simple Calculator
+const num1 = 20;
+const num2 = 10;
+
+console.log(`The Answer of SUM is =`, num1+num2);
+console.log(`The Answer of SUbTRACTION is =`, num1-num2);
+console.log(`The Answer of DIVISION is =`, num1/num2);
+console.log(`The Answer of MULTIPLICATION is=`, num1*num2);
